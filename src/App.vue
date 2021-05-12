7122829b0fe3fa9bc5c10c4cb2941ea7b7b29c30
Vitaliy <vit4ly.kiselev@gmail.com>
2021-05-13 00:14:32 +0300

    добавил спинер

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="container mx-auto flex flex-col items-center  p-4">
+    <div v-if="loading" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
+
     <div class="container">
       <section>
         <div class="flex">
@@ -154,9 +161,14 @@ export default {
       ticker: '',
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+      loading: false
     }
   },
+  created() {
+    this.loadPage()
+  },
+
   methods: {
     add() {
       const currentTicker = {name: this.ticker.toUpperCase(), price: '-'}
@@ -185,6 +197,10 @@ export default {
       const maxValue = Math.max(...this.graph)
       const minValue = Math.min(...this.graph)
       return this.graph.map( price => 5 + ((price - minValue ) * 95) / (maxValue - minValue))
+    },
+    loadPage() {
+      this.loading = true
+      setTimeout(() => this.loading = false, 2000)
     }
 
   }
